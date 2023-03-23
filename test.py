f250422799083005628b03a59732a3848d5ca67e
killeent1 <tkilleen90@gmail.com>
2023-03-23 10:48:42 +0000

    tidy up on code

--- test.py
+++ test.py
@@ -7,9 +7,8 @@ from pprint import pprint
 #hostname = input("Device hostname: ")
 #junos_username = input("JUNOS username: ")
 junos_username = "tkilleen"
-
-hostname = ""
 hosts = []
+
 '''open the hosts file for ssh access'''
 try:
     f = open("hosts.txt", "r")
@@ -20,14 +19,17 @@ try:
 except FileNotFoundError:
     print("An exception occurred: File not found!")
 
-print(hosts)
-
 for h in hosts:
-    junos_password = getpass("JUNOS or SSH key password for : {}".format(h))
+    junos_password = getpass("Password for : {}\n".format(h))
     try:
         dev = Device(host=h, user=junos_username, passwd=junos_password)
         dev.open()
         print(dev.connected)
+        pprint(dev.facts)
+        
+        dev.close()
+        print(dev.connected)
+    
     except ConnectError as err:
         print("Cannot connect to device: {0}".format(err))
         sys.exit(1)
@@ -35,6 +37,4 @@ for h in hosts:
         print(err)
         sys.exit(1)
 
-    pprint(dev.facts)
-    dev.close()
-    print(dev.connected)
+print("end of script")
