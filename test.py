5d77ebd34b223d025d87f8547e3d4f000e5a7a62
killeent1 <tkilleen90@gmail.com>
2023-03-23 10:31:26 +0000

    adding a loop mechanism

--- test.py
+++ test.py
@@ -4,28 +4,37 @@ from jnpr.junos.exception import ConnectError
 from getpass import getpass
 from pprint import pprint
 
-f = open("hosts.txt", "r")
-for x in f:
-    print(x)
-f.close()
+#hostname = input("Device hostname: ")
+#junos_username = input("JUNOS username: ")
+junos_username = "tkilleen"
 
-hostname = input("Device hostname: ")
-junos_username = input("JUNOS username: ")
-junos_password = getpass("JUNOS or SSH key password: ")
-
-#dev = Device(host='10.0.1.253', user='tkilleen', password='Cog@Lg1FGAVH:^T/%0g9')
-dev = Device(host=hostname, user=junos_username, passwd=junos_password)
-
+hostname = ""
+hosts = []
+'''open the hosts file for ssh access'''
 try:
-    dev.open()
-    print(dev.connected)
-except ConnectError as err:
-    print("Cannot connect to device: {0}".format(err))
-    sys.exit(1)
-except Exception as err:
-    print(err)
-    sys.exit(1)
+    f = open("hosts.txt", "r")
+    for x in f:
+        print(x)
+        hosts.append(x.rstrip())
+    f.close()
+except FileNotFoundError:
+    print("An exception occurred: File not found!")
 
-pprint(dev.facts)
-dev.close()
-print(dev.connected)
+print(hosts)
+
+for h in hosts:
+    junos_password = getpass("JUNOS or SSH key password for : {}".format(h))
+    try:
+        dev = Device(host=h, user=junos_username, passwd=junos_password)
+        dev.open()
+        print(dev.connected)
+    except ConnectError as err:
+        print("Cannot connect to device: {0}".format(err))
+        sys.exit(1)
+    except Exception as err:
+        print(err)
+        sys.exit(1)
+
+    pprint(dev.facts)
+    dev.close()
+    print(dev.connected)
